1913ddbba36a04f0055d17d10e26b8f2789364f6
jacgit18 <jacdevgoo2020@gmail.com>
2021-10-24 17:29:03 -0400

    rm methods

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import Particles from "react-tsparticles";
 
 
 import Register from '../Components/Register/Register';
@@ -16,17 +15,6 @@ import '../style/App.css';
 
 
 
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 30,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       }
-//     }
-//   }
-// }
 const initialState = {
       input: '',
       imageUrl: '',
@@ -42,15 +30,7 @@ const initialState = {
       }
 }
 
-// const particlesInit = (main) => {
-//   console.log(main);
 
-//   // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-// };
-
-// const particlesLoaded = (container) => {
-//   console.log(container);
-// };
 class App extends Component {
   constructor() {
     super();
@@ -158,95 +138,7 @@ class App extends Component {
 
     return (
       <div className="App ">
-       {/* <Particles className='particles'
-          params={particlesOptions}
-        /> */}
-
-        
-
-        {/* <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        background: {
-          color: {
-            value: "#0d47a1",
-          },
-        },
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: "canvas",
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
-    /> */}
+      
 
         <Banner />
 
